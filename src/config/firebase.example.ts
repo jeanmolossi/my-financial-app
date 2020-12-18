c3139f25eb9fbd21830a502aa8c6e1e3b4358a56
jeanmolossi <jean.carlosmolossi@gmail.com>
2020-12-18 01:51:06 -0300

    doc: config file example

---
+++ src/config/firebase.example.ts
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "apiKey",
+  authDomain: "authDomain",
+  projectId: "projectId",
+  storageBucket: "storageBucket",
+  messagingSenderId: "messagingSenderId",
+  appId: "appId",
+  measurementId: "measurementId2"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
