37fd7f81cb49e26745c344376015c74722aff33e
jeanmolossi <jean.carlosmolossi@gmail.com>
2020-12-28 03:36:01 -0300

    remove: removed edit transaction unfeature screen

--- src/screens/EditTransaction/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components/native';
-
-export const SelectContainer = styled.View`
-  margin: 8px 16px;
-  padding: 0 8px;
-
-  border: 1px solid #1f204125;
-  border-radius: 4px;
-`;
